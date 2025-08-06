40e582d807b9c049bf3366ea08181b01d2c1bc06
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-08-06 10:41:28 -0300

    dthgdt

--- login/script.js
+++ login/script.js
@@ -4,3 +4,15 @@ function login(){
 
     console.log(nome, senha)
 }
+function showPassword(){
+    var input_senha = document.querySelector('#senha')
+    var img_eye = document.querySelector('#eye')
+
+    if(input_senha.getAttribute('type') === 'password'){
+        input_senha.setAttribute('type','text')
+        img_eye.setAttribute('src', './Images/eye.png')
+    }else{
+        input_senha.setAttribute('type', 'password')
+        img_eye.setAttribute('src', './Images/eye-not.png')
+    }
+}
